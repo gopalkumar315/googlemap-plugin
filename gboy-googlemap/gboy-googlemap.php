71a13074c49ab6ba13b5a19cfba9135cafa97091
Gopal Kumar <gopalkumar315@gmail.com>
2017-09-06 01:45:50 +0530

    add readme.txt

--- gboy-googlemap/gboy-googlemap.php
+++ gboy-googlemap/gboy-googlemap.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Copyright (c) 2017.
- * Plugin Name: Google Map
- * Plugin URI: http://googleboy.in
+ * Plugin Name: Gboy Custom Google Map
+ * Plugin URI: https://github.com/gopalkumar315/googlemap-plugin
  * Description: Stylish Google Map
  * Version: 1.0
  * Author: Gopal Kumar
